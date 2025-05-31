6062edc55f9232a63d28c7b0b7bc2878c3e2ef4e
Het <hp985383@gmail.com>
2025-05-31 08:25:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+window.onload = function() {
+    // Retrieve browser name and version from the navigator object
+    var browserName = navigator.appName; // Get the browser name
+    var version = navigator.appVersion;   // Get the browser version
+
+    // Format the message
+    var message = "You are using " + browserName + " version " + version;
+
+    // Display the message in the div
+    document.getElementById("browser-info").innerText = message;
+};
